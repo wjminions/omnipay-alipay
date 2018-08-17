3f563a7b6bd9fea2a33d515628fea72ea9078dec
wjminions <sz_022@outlook.com>
2018-08-17 15:19:09 +0800

    1.0.7

---
+++ src/Message/MobileCompletePurchaseRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Omnipay\Alipay\Message;
+
+use Omnipay\Common\Message\ResponseInterface;
+use Omnipay\Alipay\Helper;
+
+/**
+ * Class MobileCompletePurchaseRequest
+ * @package Omnipay\Alipay\Message
+ */
+class MobileCompletePurchaseRequest extends AbstractAppRequest
+{
+    public function getData()
+    {
+        return $this->getRequestParams();
+    }
+
+
+    public function setRequestParams($value)
+    {
+        $this->setParameter('request_params', $value);
+    }
+
+
+    public function getRequestParams()
+    {
+        return $this->getParameter('request_params');
+    }
+
+
+    /**
+     * Send the request with specified data
+     *
+     * @param  mixed $data The data to send
+     *
+     * @return ResponseInterface
+     */
+    public function sendData($data)
+    {
+        $aop = new \AopClient;
+        $aop->alipayrsaPublicKey = $this->getAlipayRsaPublicKey();
+        $flag = $aop->rsaCheckV1($_POST, NULL, $this->getSignType());
+
+        $data['is_paid'] = false;
+        if ($flag && $data['trade_status'] == 'TRADE_SUCCESS') {
+            // 验证通过
+            $data['is_paid'] = true;
+
+            http_response_code(200);
+            echo 'success';
+        }
+
+        return $this->response = new MobileCompletePurchaseResponse($this, $data);
+    }
+}
